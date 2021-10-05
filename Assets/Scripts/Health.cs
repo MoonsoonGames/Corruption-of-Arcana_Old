27bde9d66360fe85b7822a12b53d0e72c1ffd79f
ANDREW SCOTT <as243879@falmouth.ac.uk>
2021-10-05 17:36:46 +0100

    Setup for card parent.
    UML Diagrams in progress.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 1;
+
+    private void Start()
+    {
+        Cards card;
+
+        card.
+    }
+}
