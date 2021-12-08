febd9579366ce5cad7acec99f5457be348c0f03a
ANDREW SCOTT <as243879@falmouth.ac.uk>
2021-12-08 11:13:39 +0000

    fix conflicts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,15 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     CharacterController characterController;
-    Rigidbody rb;
 
-    public float baseMoveSpeed = 50f;
-    public float baseSprintSpeed = 80f;
+    public float baseMoveSpeed;
+    public float baseSneakSpeed;
+    public float baseSprintSpeed;
     float moveSpeed;
 
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
 
-    private float turnCamera;
     public float sensitivity = 5;
 
     public int maxHealth = 50;
@@ -24,6 +23,8 @@ public class PlayerController : MonoBehaviour
     public int arcana;
     public Slider healthBar;
     public Slider arcanaBar;
+
+    public Text Location;
 
     private Vector3 moveDirection = Vector3.zero;
 
@@ -43,14 +44,11 @@ public class PlayerController : MonoBehaviour
 
     public GameObject interactImage;
 
-    bool canMove = false;
-
     void Start()
     {
         //Load position
         moveSpeed = baseMoveSpeed;
         characterController = GetComponent<CharacterController>();
-        rb = GetComponent<Rigidbody>();
 
         sceneLoader = GameObject.FindObjectOfType<SceneLoader>();
 
@@ -79,10 +77,10 @@ public class PlayerController : MonoBehaviour
                     potionCount = loadSettings.potionCount;
                 }
 
-                Vector3 spawnPos = loadSettings.RequestPosition(this, SceneManager.GetActiveScene().name);
+                Vector3 spawnPos = loadSettings.RequestPosition(this);
 
                 SetupTransform(spawnPos);
-                StartCoroutine(IDelayStartTransform(2f, spawnPos));
+                StartCoroutine(IDelayStartTransform(1f, spawnPos));
             }
             else
             {
@@ -93,16 +91,14 @@ public class PlayerController : MonoBehaviour
 
         loadSettingsArray = GameObject.FindObjectsOfType<LoadSettings>();
 
-        //Debug.Log("Length: " + loadSettingsArray.Length);
+        Debug.Log("Length: " + loadSettingsArray.Length);
         //Debug.Break();
     }
 
     IEnumerator IDelayStartTransform(float delay, Vector3 newSpawnPos)
     {
         yield return new WaitForSeconds(delay);
-        //Debug.Log("Should be able to move");
         SetupTransform(newSpawnPos);
-        canMove = true;
     }
 
     void SetupTransform(Vector3 targetPosition)
@@ -112,66 +108,62 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (canMove)
+        if (characterController.isGrounded)
         {
-            if (characterController.isGrounded)
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= moveSpeed;
+
+            if (Input.GetButton("Jump"))
             {
-                // We are grounded, so recalculate
-                // move direction directly from axes
-
-                moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-                moveDirection = transform.TransformDirection(moveDirection);
-                moveDirection *= moveSpeed;
-
-                if (Input.GetButton("Jump"))
-                {
-                    moveDirection.y = jumpSpeed;
-                }
-
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    moveSpeed = baseSprintSpeed;
-                }
-
-                else
-                {
-                    moveSpeed = baseMoveSpeed;
-                }
+                moveDirection.y = jumpSpeed;
             }
 
-            // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-            // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-            // as an acceleration (ms^-2)
-            moveDirection.y -= gravity * Time.deltaTime;
-
-            // Move the controller
-            characterController.Move(moveDirection * Time.deltaTime);
-            //rb.velocity += moveDirection;
-
-            //turnCamera = Input.GetAxis("Mouse X") * sensitivity;
-            //if (turnCamera != 0)
-            //{
-            //    //Code for action on mouse moving horizontally
-            //    transform.eulerAngles += new Vector3(0, turnCamera, 0);
-            //}
-
-            if (Input.GetButton("Interact") && interact && dialogue != null)
+            if (Input.GetKey(KeyCode.LeftShift))
             {
-                dialogue.LoadScene();
+                moveSpeed = baseSprintSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                moveSpeed = baseSneakSpeed;
+            }
+
+            else
+            {
+                moveSpeed = baseMoveSpeed;
             }
         }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
 
         //Sets the values of the healthbars to their specific values
         if (healthBar != null)
             healthBar.value = health;
         if (arcanaBar != null)
             arcanaBar.value = arcana;
+
+
+        if (Input.GetButton("Interact") && interact && dialogue != null)
+        {
+            dialogue.LoadScene();
+        }
     }
 
     public void OnTriggerEnter(Collider other)
     {
         //Save current position
-        SavePlayerPos();
+        if (loadSettings != null)
+            loadSettings.playerPosInThoth = transform.position;
 
         if (other.gameObject.CompareTag("commonEnemy") || other.gameObject.CompareTag("bossEnemy"))
         {
@@ -185,7 +177,7 @@ public class PlayerController : MonoBehaviour
 
         else if (other.gameObject.CompareTag("NPC"))
         {
-            //Debug.Log("Can Interact");
+            Debug.Log("Can Interact");
             interact = true;
             dialogue = other.gameObject.GetComponent<Dialogue>();
 
@@ -194,23 +186,39 @@ public class PlayerController : MonoBehaviour
                 interactImage.SetActive(true);
             }
         }
-    }
 
-    public void SavePlayerPos()
-    {
-        string scene = SceneManager.GetActiveScene().name;
-        if (loadSettings != null)
+        #region Thoth location triggers
+        if (other.gameObject.CompareTag("Thoth Mid City"))
         {
-            if (scene == E_Levels.Thoth.ToString())
-            {
-                loadSettings.playerPosInThoth = transform.position;
-            }
-            else if (scene == E_Levels.Clearing.ToString())
-            {
-                loadSettings.playerPosInClearing = transform.position;
-            }
+            Location.text = "Thoth - MidCity".ToString();
         }
-            
+        else if (other.gameObject.CompareTag("Thoth Market"))
+        {
+            Location.text = "Thoth - Market".ToString();
+        }
+        else if (other.gameObject.CompareTag("Thoth Bridge"))
+        {
+            Location.text = "Thoth - Bridge".ToString();
+        }
+        else if (other.gameObject.CompareTag("Thoth East housing"))
+        {
+            Location.text = "Thoth - East houses".ToString();
+        }
+        else if (other.gameObject.CompareTag("Thoth West housing"))
+        {
+            Location.text = "Thoth - West houses".ToString();
+        }
+        else if (other.gameObject.CompareTag("Thoth Open Sea"))
+        {
+
+        }
+
+        #endregion
+
+        #region Clearing location triggers
+
+        #endregion
+
     }
 
     public void OnTriggerExit(Collider other)
@@ -248,13 +256,6 @@ public class PlayerController : MonoBehaviour
         {
             potionCount = Mathf.Clamp(potionCount + value, 0, maxPotions);
         }
-
-        if (potionCount == 0)
-        {
-            //combatManager.HealingItem.SetActive(false);
-        }
-
-        //combatManager.HealingLeft.text = potionCount.ToString();
 
         if (loadSettings != null)
         {
