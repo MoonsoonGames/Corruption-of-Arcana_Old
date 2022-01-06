73e2b95dfb75e72a368de37373e72b0665ed407e
ANDREW SCOTT <as243879@falmouth.ac.uk>
2022-01-06 12:43:24 +0000

    Dialogue fixes

--- Assets/Characters/Dialogue.cs
+++ Assets/Characters/Dialogue.cs
@@ -22,6 +22,8 @@ public class Dialogue : MonoBehaviour
 
     public Object dialogue;
 
+    public LoadSceneMode sceneMode;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -60,7 +62,7 @@ public class Dialogue : MonoBehaviour
                     }
                 }
 
-                sceneLoader.LoadSpecifiedScene(sceneString, LoadSceneMode.Single, dialogue);
+                sceneLoader.LoadSpecifiedScene(sceneString, sceneMode, dialogue);
             }
         }
     }
