f3ea6d440adda973212d5f6159dac42cf536e72e
ANDREW SCOTT <as243879@falmouth.ac.uk>
2022-01-25 12:28:29 +0000

    Lake boss fight now requires the well fight to have been completed.

--- Assets/Scenes/DestroyObject.cs
+++ Assets/Scenes/DestroyObject.cs
@@ -6,14 +6,38 @@ public class DestroyObject : MonoBehaviour
 {
     LoadSettings loadSettings;
 
+    public string[] enemiesRequired;
+    public bool requireAll = true;
+
+    public bool destroyIfContains = true;
+
     // Start is called before the first frame update
     void Start()
     {
         loadSettings = GameObject.Find("LoadSettings").GetComponent<LoadSettings>();
 
-        if (loadSettings.enemiesKilled.Contains("CrystalFight"))
+        bool contains1 = false;
+        bool containsAll = true;
+
+        foreach (var item in enemiesRequired)
+        {
+            if (loadSettings.enemiesKilled.Contains(item))
+            {
+                contains1 = true;
+            }
+            else
+            {
+                containsAll = false;
+            }
+        }
+
+        if ((((requireAll && containsAll) || (!requireAll && contains1)) && destroyIfContains) || (((!requireAll && !containsAll) || (requireAll && !contains1)) && !destroyIfContains))
         {
             Destroy(this.gameObject);
+        }
+        else
+        {
+            
         }
     }
 }
