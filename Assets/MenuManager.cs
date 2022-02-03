7a3f3da580ec05fde75c0d4673b08775aabeede9
JACK DRAGE <JD246711@falmouth.ac.uk>
2022-02-03 18:02:44 +0000

    More work on the new pause/inventory Menu New potion art has been added

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
@@ -21,7 +22,10 @@ public class MenuManager : MonoBehaviour
 
     public GameObject SettingsMenu;
     public GameObject HelpMenu;
+    public GameObject PlayerStatsUI;
 
+    public GameObject MainMenuConfirmScreen;
+    public GameObject QuitConfirmScreen;
     #endregion
 
     // Start is called before the first frame update
@@ -31,11 +35,15 @@ public class MenuManager : MonoBehaviour
         PauseMenuUI.SetActive(false);
         //Exploration UI ON
         ExplorationUI.SetActive(true);
+        //hide/lock mouse
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        #region Open Hotkeys
         //Esc to OPEN menu
         if (PauseMenuUI.activeSelf == false && Input.GetKeyDown(KeyCode.Escape))
         {
@@ -43,6 +51,8 @@ public class MenuManager : MonoBehaviour
             ExplorationUI.SetActive(false);
             //turn on Pause Menu UI
             PauseMenuUI.SetActive(true);
+            //turn on player Stats
+            PlayerStatsUI.SetActive(true);
 
             //Freeze player and camera
             Player.GetComponent<PlayerController>().canMove = false;
@@ -50,8 +60,20 @@ public class MenuManager : MonoBehaviour
             //unlock mouse - not confined
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
-        }
 
+            //Debug
+            Debug.Log("Pause Menu Active");
+        }
+        //M to open local map
+            //turn off Exploration UI
+            //turn on local map
+            //freeze player and camera
+            //keep mouse hidden/locked in place
+            //Debug
+
+        #endregion
+
+        #region Close Hotkeys
         //Esc to CLOSE menu
         if (PauseMenuUI.activeSelf == true && Input.GetKeyDown(KeyCode.Escape))
         {
@@ -62,12 +84,22 @@ public class MenuManager : MonoBehaviour
             //Unfreeze player and camera
             Player.GetComponent<PlayerController>().canMove = true;
 
-            //hide mouse/lock in place
+            //hide mouse/locked in place
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
-        }
 
+            //Debug
+            Debug.Log("Pause Menu Deactive");
+        }
+        //M to close local map
+            //turn on Exploration UI
+            //turn off local map
+            //unfreeze player and camera
+            //keep mouse hidden/locked in place
+            //Debug
+        #endregion
     }
+
     #region Pause Menu Buttons
 
     #region Resume Button
@@ -108,14 +140,41 @@ public class MenuManager : MonoBehaviour
     #region MainMenu Button
     public void MainMenu()
     {
+        //Load Confirm Screen
+        MainMenuConfirmScreen.SetActive(true);
+
+    }
+    public void MainMenuConfirm()
+    {
         //Return to main menu
+        SceneManager.LoadScene("Splash Screen");
+    }
+    public void MainMenuDeny()
+    {
+        //Close Quit confirmation screen
+        MainMenuConfirmScreen.SetActive(false);
     }
     #endregion
 
     #region CloseGame Button
     public void CloseGame()
     {
+        //Load confirm screen
+        QuitConfirmScreen.SetActive(true);
+    }
+
+    public void CloseConfirm()
+    {
+        //Quit Game
         Application.Quit();
+        //Debug
+        Debug.Log("Closed Game");
+    }
+
+    public void CloseDeny()
+    {
+        //Close Quit confirmation screen
+        QuitConfirmScreen.SetActive(false);
     }
     #endregion
 
@@ -128,4 +187,5 @@ public class MenuManager : MonoBehaviour
     #endregion
 
     #endregion
+
 }
